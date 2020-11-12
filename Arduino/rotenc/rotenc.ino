6c2a9d5c3b667081a6186f79dfa147cc2be1a765
jacob-jarick <mem.namefix@gmail.com>
2020-11-13 00:04:52 +0800

    v2 keyboard

--- Arduino/rotenc/rotenc.ino
+++ Arduino/rotenc/rotenc.ino
@@ -4,6 +4,12 @@
     Written by Jacob Jarick
     mem.namefix@gmail.com
    https://github.com/jacob-jarick/usb-rotary-keyboard
+
+   board: sparkfun pro micro
+   5v 16Mhz
+   board arduino git repo:
+   https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
+
 */
 
 #include "HID-Project.h"
@@ -11,12 +17,20 @@
 
 CommonBusEncoders encoders(4, 6, 8, 4);
 
+uint8_t keya = 19;
+uint8_t keyb = 7;
+uint8_t keyc = 22;
+uint8_t keyd = 5;
+
+uint8_t key_debounce = 150;
+
+unsigned long keysleep[4] = {0, 0, 0, 0};
+
 // set to match your encoders.
 uint8_t rsteps = 2;
 
 // these bools will be set via jumpers
 bool alt_steps = 1; // 0 = 2 steps, 1 = 4 steps
-bool fliprots = 1; // rotate assignments 180
 bool rmouse = 0; // worlds most awkward mouse - etch a sketch style
 
 // internal bools
@@ -25,26 +39,21 @@ bool mousepress = 0;
 
 void setup()
 {
+  pinMode(keya, INPUT);
+  pinMode(keyb, INPUT);
+  pinMode(keyc, INPUT);
+  pinMode(keyd, INPUT);
+
   encoders.setDebounce(16);
   encoders.resetChronoAfter(1000);
 
   if (alt_steps)
     rsteps = 4;
 
-  if (!fliprots)
-  {
-    encoders.addEncoder(1, rsteps, 9, 1, 100,  199);
-    encoders.addEncoder(2, rsteps, 10, 1, 200, 299);
-    encoders.addEncoder(3, rsteps, 11, 1, 300,  399);
-    encoders.addEncoder(4, rsteps, 12, 1, 400, 499);
-  }
-  else
-  {
-    encoders.addEncoder(1, rsteps, 12, 1, 100,  199);
-    encoders.addEncoder(2, rsteps, 11, 1, 200, 299);
-    encoders.addEncoder(3, rsteps, 10, 1, 300,  399);
-    encoders.addEncoder(4, rsteps, 9, 1, 400, 499);
-  }
+  encoders.addEncoder(1, rsteps, 12, 1, 100,  199);
+  encoders.addEncoder(2, rsteps, 11, 1, 200, 299);
+  encoders.addEncoder(3, rsteps, 10, 1, 300,  399);
+  encoders.addEncoder(4, rsteps, 9, 1, 400, 499);
 
 
   Consumer.begin();
@@ -58,21 +67,59 @@ void setup()
 
 void loop()
 {
+  // read keys
+  if (keysleep[0] < millis() && !digitalRead(keya))
+  {
+    keysleep[0] = millis() + key_debounce;
+    Serial.println("A");
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('c');
+    delay(100);
+    Keyboard.releaseAll();
+  }
+  if (keysleep[1] < millis() && !digitalRead(keyb))
+  {
+    keysleep[1] = millis() + key_debounce;
+    Serial.println("B");
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('x');
+    delay(100);
+    Keyboard.releaseAll();
+  }
+  if (keysleep[2] < millis() && !digitalRead(keyc))
+  {
+    keysleep[2] = millis() + key_debounce;
+    Serial.println("C");
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('v');
+    delay(100);
+    Keyboard.releaseAll();
+  }
+  if (keysleep[3] < millis() && !digitalRead(keyd))
+  {
+    keysleep[3] = millis() + key_debounce;
+    Keyboard.write(KEY_RETURN);
+    Serial.println("D");
+  }
+
+
   int index = encoders.readAll();
   if (index == 0)
     return;
 
   Serial.println(index);
 
+
+
   //ROT 1
-  if (index == 100)
+  if (index == 101)
   {
     if (rmouse)
       Mouse.move(5, 0, 0);
     else
       Consumer.write(MEDIA_VOLUME_UP);
   }
-  else if (index == 101)
+  else if (index == 100)
   {
     if (rmouse)
       Mouse.move(-5, 0, 0);
@@ -88,14 +135,14 @@ void loop()
   }
 
   // ROI 2
-  else if (index == 200)
+  else if (index == 201)
   {
     if (rmouse)
       Mouse.move(0, 5, 0);
     else
       Consumer.write(MEDIA_NEXT);
   }
-  else if (index == 201)
+  else if (index == 200)
   {
     if (rmouse)
       Mouse.move(0, -5, 0);
@@ -114,7 +161,7 @@ void loop()
   }
 
   // ROT 3
-  else if (index == 300)
+  else if (index == 301)
   {
     if (rmouse)
       Mouse.move(5, 5, 0);
@@ -122,7 +169,7 @@ void loop()
 
       Consumer.write(MEDIA_FAST_FORWARD);
   }
-  else if (index == 301)
+  else if (index == 300)
   {
     if (rmouse)
       Mouse.move(-5, -5, 0);
@@ -140,14 +187,14 @@ void loop()
       Consumer.write(MEDIA_STOP);
   }
 
-  else if (index == 400)
+  else if (index == 401)
   {
     if (rmouse)
       Mouse.move(-5, 5, 0);
     else
       Keyboard.write(KEY_RIGHT_ARROW);
   }
-  else if (index == 401)
+  else if (index == 400)
   {
     if (rmouse)
       Mouse.move(5, -5, 0);
--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 Keyboard mode Mappings:
 
-
 | Rotary | Left           | Right           | Click      |
 |--------|----------------|-----------------|------------|
 | 1      | Volume Down    | Volume Up       | Mute       |
@@ -19,3 +18,12 @@ Mouse mode Mappings:
 | 2      | Down     | Up         | Click         |
 | 3      | Left Up  | Right Down | Right Click   |
 | 4      | Right Up | Left Down  | Middle Click  |
+
+Cherry MX Key Functions
+
+| Key | Function       |
+|-----|----------------|
+| 1   | Copy (CTRL+C)  |
+| 2   | Cut (CTRL+X)   |
+| 3   | Paste (CTRL+V) |
+| 4   | Enter          |
