302c9f744c3c19b827bee2d5c692f201868122c7
jacob-jarick <mem.namefix@gmail.com>
2020-11-19 23:25:04 +0800

    Update rotenc.ino

--- Arduino/rotenc/rotenc.ino
+++ Arduino/rotenc/rotenc.ino
@@ -16,15 +16,6 @@
 #include <CommonBusEncoders.h>
 CommonBusEncoders encoders(4, 6, 8, 4);
 
-// https://forum.arduino.cc/index.php?topic=324554.0
-
-#define KEY_F13  0xF0 // 0x68 + 0x88
-#define KEY_F14 0xF1 // 0x69 + 0x88
-#define KEY_F15 0xF2 // 0x6A + 0x88
-#define KEY_F16 0xF3 // 0x6B + 0x88
-#define KEY_F17 0xF4 // 0x6C + 0x88
-#define KEY_F18 0xF5 // 0x6D + 0x88
-
 const uint8_t keya = 19;
 const uint8_t keyb = 7;
 const uint8_t keyc = 22;
@@ -71,100 +62,63 @@ void setup()
   encoders.addEncoder(3, rsteps, 10, 1, 300,  399);
   encoders.addEncoder(4, rsteps, 9, 1, 400, 499);
 
-
   Consumer.begin();
   Keyboard.begin();
   Mouse.begin();
 
-  Serial.begin(9600);
-  Serial.println(F("USB Rotary Keyboard v1"));
-  Serial.println(F("Created by Jacob Jarick"));
+  Serial.begin(19200);
 }
 
 void loop()
 {
+  // ====================================================
+  // read dip switches
+
   rmouse = digitalRead(dip2);
   uint8_t alt_keymap = digitalRead(dip1);
 
+  // ====================================================
   // read keys
+
   if (keysleep[0] < millis() && !digitalRead(keya))
   {
     keysleep[0] = millis() + key_debounce;
 
     if (alt_keymap)
-    {
-      Serial.println("A - Copy");
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press('c');
-      Keyboard.releaseAll();
-    }
+      macro_copy();
     else
-    {
-      Serial.println("F13");
-      Keyboard.write(KEY_F13);
-    }
+      macro_shift_fx(1);
   }
   if (keysleep[1] < millis() && !digitalRead(keyb))
   {
     keysleep[1] = millis() + key_debounce;
 
     if (alt_keymap)
-    {
-      Serial.println("B - Cut");
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press('x');
-      Keyboard.releaseAll();
-    }
+      macro_cut();
     else
-    {
-      Serial.println("F14");
-      Keyboard.write(KEY_F14);
-    }
+      macro_shift_fx(2);
   }
   if (keysleep[2] < millis() && !digitalRead(keyc))
   {
     keysleep[2] = millis() + key_debounce;
 
     if (alt_keymap)
-    {
-      Serial.println("C - Paste");
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press('v');
-      Keyboard.releaseAll();
-    }
+      macro_paste();
     else
-    {
-      Serial.println("F15");
-      Keyboard.write(KEY_F15);
-    }
+      macro_shift_fx(3);
   }
   if (keysleep[3] < millis() && !digitalRead(keyd))
   {
     keysleep[3] = millis() + key_debounce;
 
     if (alt_keymap)
-    {
-      Serial.println("D - Paste Special");
-
-      Keyboard.write(KEY_HOME);
-
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_END);
-      Keyboard.releaseAll();
-
-      Keyboard.write(KEY_DELETE);
-
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press('v');
-      Keyboard.releaseAll();
-    }
+      macro_paste_special();
     else
-    {
-      Serial.println("F16");
-      Keyboard.write(KEY_F16);
-    }
+      macro_shift_fx(4);
   }
 
+  // ====================================================
+  // read encoders
 
   int index = encoders.readAll();
   if (index == 0)
@@ -172,9 +126,9 @@ void loop()
 
   Serial.println(index);
 
+  // ----------------------------------------
+  // Rotary 1 - media vol, up, down, mute
 
-
-  //ROT 1
   if (index == 101)
   {
     if (rmouse)
@@ -197,7 +151,9 @@ void loop()
       Consumer.write(MEDIA_VOLUME_MUTE);
   }
 
-  // ROI 2
+  // ----------------------------------------
+  // Rotary 2 - media, next, prev, play/pause
+
   else if (index == 201)
   {
     if (rmouse)
@@ -223,7 +179,9 @@ void loop()
       Consumer.write(MEDIA_PLAY_PAUSE);
   }
 
-  // ROT 3
+  // ----------------------------------------
+  // Rotary 3 - media ffwd, rwd, stop
+
   else if (index == 301)
   {
     if (rmouse)
@@ -249,6 +207,9 @@ void loop()
     else
       Consumer.write(MEDIA_STOP);
   }
+
+  // ----------------------------------------
+  // Rotary 4 - mplayer control
 
   else if (index == 401)
   {
@@ -284,4 +245,87 @@ void mousetoggle()
     Mouse.press();
   else
     Mouse.release();
+}
+
+
+void macro_paste()
+{
+  Serial.println("C - Paste");
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('v');
+  delay(50);
+  Keyboard.releaseAll();
+}
+
+void macro_cut()
+{
+  Serial.println("B - Cut");
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('x');
+  delay(50);
+  Keyboard.releaseAll();
+}
+
+void macro_copy()
+{
+  Serial.println("A - Copy");
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('c');
+  delay(50);
+  Keyboard.releaseAll();
+}
+
+void macro_paste_special()
+{
+  Serial.println("D - Paste Special");
+
+  // jump to begining of form input/ line
+  Keyboard.write(KEY_HOME);
+  delay(50);
+
+  // select contents of form input/ line
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(KEY_END);
+  delay(50);
+
+  Keyboard.releaseAll();
+  delay(50);
+
+  // delete contents
+  Keyboard.write(KEY_DELETE);
+  delay(50);
+
+  // paste clipboard contents
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('v');
+  delay(50);
+  Keyboard.releaseAll();
+}
+
+void macro_shift_fx(uint8_t fx)
+{
+  Serial.println("F" + String(fx));
+  
+  Keyboard.press(KEY_LEFT_SHIFT);
+
+  if (fx == 1)
+    Keyboard.press(KEY_F1);
+  else if (fx == 2)
+    Keyboard.press(KEY_F2);
+  else if (fx == 3)
+    Keyboard.press(KEY_F3);
+  else if (fx == 4)
+    Keyboard.press(KEY_F4);
+
+  delay(50);
+  Keyboard.releaseAll();
+}
+
+void macro_copy()
+{
+  Serial.println("A - Copy");
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('c');
+  delay(50);
+  Keyboard.releaseAll();
 }
